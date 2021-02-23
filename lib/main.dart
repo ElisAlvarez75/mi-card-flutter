8133e4fd4f7df2b06ab96d9ed1e9e5e256fed874
Elis Alvarez <elisalvarez@msn.com>
2021-02-23 14:09:32 +0100

    App almost finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Center(child: MyApp()));
@@ -20,11 +21,64 @@ class MyApp extends StatelessWidget {
               ),
               Text(
                 'Elis',
-                style: TextStyle(
+                style: GoogleFonts.pacifico(
                     fontSize: 40.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
               ),
+              Text(
+                'Flutter Developer',
+                style: GoogleFonts.sourceSansPro(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '649 887 259',
+                      style: GoogleFonts.sourceSansPro(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'elisalvarez@msn.com',
+                      style: GoogleFonts.sourceSansPro(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
